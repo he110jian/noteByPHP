f312d802cc2322f8f72b1b3597a8470e6300f2c8
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 14:26:43 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/head.php

---
+++ 1/head.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Created on 2011-5-30
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+  <a href="add.php">лМ╪саТят</a>||
+  <a href="list.php">аТятап╠М</a>||
+
+  <a href="login.php">╣гб╪</a>
+
+
+
+  <hr>
