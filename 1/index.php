43e39ca07e782a152e9774fd9e334aee8504dbf1
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 14:03:41 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -11,7 +11,7 @@
 <link href="js/type.css" type="text/css" rel="stylesheet"  />
 </head>
 
-<body>
+    <body background-image:url(../js/1.jpg)>
 	<div>
         <h3 class="type" style="background-color:#6899CE"><a href="javascript:void(0)">activity</a></h3>
         <div class="content">
