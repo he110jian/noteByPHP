5db9644aa3af00101df9ef8736460920ec69da15
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 13:38:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -3,4 +3,6 @@ echo "<h1><blink>hello!</h1>";
 ?>
 
 <body bgcolor="#339933">
+<DIV align=center><FONT style="FONT-SIZE: 24pt; FILTER: shadow(color=green direction=45); WIDTH: 100%;
+COLOR: #FFCC00; LINE-HEIGHT: 150%; FONT-FAMILY: 仿宋_GB2312"><B>立体字效果立体字效果</B></FONT></DIV>
 </body>
