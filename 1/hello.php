0dadc1ce580a8319c17de9449d337f275465f267
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 15:49:38 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/hello.php

--- 1/hello.php
+++ 1/hello.php
@@ -2,12 +2,13 @@
 <?php
 //判断登录
 include_once ("log.php");
-if ($_COOKIE['cookie'] != 'ok') {
-	echo "<meta http-equiv=\"refresh\" content=\"3;url=login.php\"> illegal!<a href=\"login.php\">return</a>...";
-} else 
+if ($_COOKIE['cookie'] != 'ok') 
 {
-    
-echo "<H1>WELCOME!<H1>"
+	echo "<meta http-equiv=\"refresh\" content=\"3;url=login.php\"> illegal!<a href=\"login.php\">return</a>...";
+} 
+else 
+{   
+echo "<H1>WELCOME!<H1>";
 
 }
 ?>
