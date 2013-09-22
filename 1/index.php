5436ec5a8055ad9461f942498d2a8367b16e21a5
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 15:44:56 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=GBK" />
-<title>HELLO</title>
+<title>Login</title>
 <script src="js/prototype.lite.js" type="text/javascript"></script>
 <link href="js/type.css" type="text/css" rel="stylesheet"  />
     </head><body style="color:white">
