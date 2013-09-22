3e15752ff13b934adad9efe3c8c02ceff6f61dcd
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 13:49:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,66 +1,116 @@
-<html>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=GBK">
-<title>div+css</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>sax</title>
 
-<style type="text/css">
-
-#wrap{
-	max-width: 1200px; 
-	margin-top: 2%;
-	margin-left: 10%;
-	margin-right: 10%;
-	font-size:0.88em;
-  <!-  background: url(divCss.gif) repeat-y 90% 0;  !>
-}
-
-header[role="banner"]{
-	background: #666;
-	}
-
-#content {
-	float: left;
-	width: 68%;
-	margin-right: 2%;
-	background: #CCC;
-	}
-
-#sidebar {
-  float: right;
-  width: 28%;
-  padding: 1%;
-  background: #999;
-  }
-
-footer[role="contentinfo"]{
-  clear: both;
-  background: #DDD;
-  }
-
-</style>
+<script src="js/prototype.lite.js" type="text/javascript"></script>
+<script src="js/moo.fx.js" type="text/javascript"></script>
+<script src="js/moo.fx.pack.js" type="text/javascript"></script>
+<link href="js/type.css" type="text/css" rel="stylesheet"  />
 </head>
-
-<body >
-<div id="wrap">
-  <header role="banner">
-    <h1>Header Goes Here</h1>
-  </header>
-  <div id="sidebar"role="complementary">
-   <p>
-LALALA	
-	</p>
-  </div>
-
-  <div id="content"role="main">
-    ... content goes here ...
+<body>
+	<div>
+        <h3 class="type" style="background-color:#6899CE"><a href="javascript:void(0)">activity</a></h3>
+        <div class="content">
 <p>
-CACACACA
-</p>
-  </div>
 
-   <footer role="contentinfo">
-    ... footer goes here ...
-  </footer>
-</div> 
+<marquee onmouseover="this.stop()" onmouseout="this.start()" height=100% scrollDelay=200 direction="up" scrollmount="7">	
+     <ul>  
+	 <li>asss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	  </ul>
+	   </MARQUEE>
+	 
+</p>	
+		</div>
+        <h4 class="type"><a href="javascript:void(0)">more1</a></h4>
+        <div class="content">
+          <p>
+           <div class="txt">
+		<ul>  
+	 <li>asss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>&nbsp<strong>·</strong>asss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>&nbsp<strong>·</strong>assssss爱上就会看到s到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>&nbsp<strong>·</strong>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <br/><li>&nbsp<strong>·</strong>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li><li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	  </ul>
+				</div>
+          </p>
+        </div>
+        <h3 class="type" style="background-color:#6899CE"><a href="javascript:void(0)">newss</a></h3>
+        <div class="content">
+        <p>
+
+		<marquee onmouseover="this.stop()" onmouseout="this.start()" height=100% scrollDelay=200 direction="up" scrollmount="7">	
+     <ul>  
+	 <li>asss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li> <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li> <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	  </ul>
+	   </MARQUEE>
+		</p>
+        </div>
+		<h4 class="type" ><a href="javascript:void(0)">more2</a></h4>
+        <div class="content">
+          <p><div class="txt">
+		  
+					<ul>  
+	 <li>asss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>asss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	 <li>assssss爱上就会看到ss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 sss爱上就会看到 ss爱上就会看到  &nbsp;&nbsp;<span style="float:right">@2011-07-121</span></li>
+	  </ul>
+				</div>
+		  </p>
+        </div>
+    </div>
+   <script type="text/javascript">
+		var contents = document.getElementsByClassName('content');
+		var toggles = document.getElementsByClassName('type');
+
+		var myAccordion = new fx.Accordion(
+			toggles, contents, {opacity: true, duration: 400,			
+			//when an element is opened change the background color to blue
+		onActive: function(id){
+			id.style.backgroundColor="#6899CE";
+		},
+		onBackground: function(id){
+			//change the background color to the original (green) 
+			//color when another toggler is pressed
+			id.style.backgroundColor="#cccccc";
+		}
+		});
+		myAccordion.showThisHideOpen(contents[0]);myAccordion.showThisHideOpen(contents[2]);
+</script>       
 </body>
 </html>
