809220775946ec62c217ef939b851a467f2727e7
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 13:42:10 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,19 +1,91 @@
-
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
- <head>
-  <title> New Document </title>
-  <meta name="Generator" content="EditPlus">
-  <meta name="Author" content="">
-  <meta name="Keywords" content="">
-  <meta name="Description" content="">
- </head>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=GBK">
+<title>div+css</title>
 
+<style type="text/css">
 
-<body bgcolor="#339933">
+#wrap{
+	max-width: 1200px; 
+	margin-top: 2%;
+	margin-left: 10%;
+	margin-right: 10%;
+	font-size:0.88em;
+  <!-  background: url(divCss.gif) repeat-y 90% 0;  !>
+}
 
-  <DIV align=center><FONT style="FONT-SIZE: 24pt; FILTER: shadow(color=green direction=45); WIDTH: 100%;
-COLOR: #FFCC00; LINE-HEIGHT: 150%"><B>HAHAHAH</B></FONT></DIV>
- </body>
+header[role="banner"]{
+	background: #666;
+	}
+
+#content {
+	float: left;
+	width: 68%;
+	margin-right: 2%;
+	background: #CCC;
+	}
+
+#sidebar {
+  float: right;
+  width: 28%;
+  padding: 1%;
+  background: #999;
+  }
+
+footer[role="contentinfo"]{
+  clear: both;
+  background: #DDD;
+  }
+
+</style>
+</head>
+
+<body >
+<div id="wrap">
+  <header role="banner">
+    <h1>Header Goes Here</h1>
+  </header>
+  <div id="sidebar"role="complementary">
+   <p>
+<ul>
+<li>12月17日 	 76人 @  奇才  季前 
+ 威瑞森中心球馆  华盛顿,  哥伦比亚特区 	 8:00 上午 			</li>
+ <li>12月17日 	 公牛 @  步行者  季前 
+ 康赛科球馆  印第安纳波利斯,  印第安纳州 	 8:00 上午 			</li>
+ <li>12月17日 	 骑士 @  活塞  季前 
+ 奥本山宫殿  底特律,  密歇根州 	 8:30 上午 			</li>
+ <li>12月17日 	 黄蜂 @  灰熊  季前 
+ 联邦快递球馆  孟菲斯,  田纳西州 	 9:00 上午 			</li>
+ <li>12月18日 	 尼克斯 @  篮网  季前 
+ 保德信中心球馆  纽瓦克,  新泽西州 	 3:00 上午 			</li>
+ <li>12月18日 	 马刺 @  火箭  季前 
+ 丰田中心球馆  休斯顿,  德克萨斯州 	 9:00 上午 			</li>
+ <li>12月18日 	 雄鹿 @  森林狼  季前 
+ 标靶中心球馆  明尼阿波利斯,  明尼苏达州 	 9:00 上午 			</li>
+ <li>12月18日 	 国王 @  勇士  季前 
+ 甲骨文球馆  奥克兰,  加利福尼亚州 	 11:30 上午 			
+ 安利球馆  奥兰多,  佛罗里达州 	 8:00 上午 			</li>
+ 
+
+ </ul>	
+	</p>
+  </div>
+
+  <div id="content"role="main">
+    ... content goes here ...
+<p>
+的字体、格式、颜色和图片，使文件的交流可以轻易跨越应用程序和系统平台的限制。但是，一些文档的特殊操作（如提取PDF文档中的一段文字或图片）对于一般用户来说，不是那么容易上手的。不过现在有了PDFTools这款神奇工具后，你就可以轻松地把PDF文档的内容转换成HTM、TXT和BMP这三种常用的文件格式。 
+为了PDFTools能更好地为我们服务，我们首先得对它进行一番设置。在主界面中点击菜单“文件→设置”或直接按“Ctrl＋D”组合键调出设置对话框。在“常规设置”标签页中设置页面的转换范围，默认为全部转换，你可以点击下拉按钮，选择“选择指定页”后，输入转换的起始页和停止页。未注册版本只能转换PDF文档的前五页内容。 
+“输出设置”框的内容是设置输入图片的像素和JPEG图片质量。选中“转换后浏览”的复选框，那么当文档转换完成后，程序就会调出对应程序打开转换后的文件。“PDF2HTM”标签页只有一“使用完全处理模式”复选框，依实际情况决定。
+六PDF转HTM 的方法 
+HTM是网页的通用格式，但PDFTools转换HTM文件后，所形成的HTM文件并不包含有PDF文档中的文字，而是将PDF文档的内容转换成JPG图像文件，然后再用HTML语言将图片嵌入到网页中。生成的HTM文件是一个框架网页，即左边是各页面的链接窗口，右边的内容就是JPG图片了。如果你用的是IE6的话，当鼠标移到右边窗口后，就会浮出一排快捷工具栏。 
+首先选择菜单“工具→PDF转HTM”，使其转为HTM模式。接着打开文件有多种操作方式：通过菜单“文件→打开”直接按“Ctrl＋F”键后调出打开对话框选择文件，或者直接把待转换的PDF文档拖到PDFTools主界面中，软件马上开始转换。转换后的文件是保存在源文件中，而且还有相应名称的子文件夹，里面就是一些JPG图片和其它资源。最后弹出一个提示窗口，点击“确定”即可。
+</p>
+  </div>
+
+   <footer role="contentinfo">
+    ... footer goes here ...
+  </footer>
+</div> 
+</body>
 </html>
