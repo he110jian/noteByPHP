d53559c8a2614289f2c2f690d0064dc5eff84ce7
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 13:40:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -16,6 +16,6 @@ echo "<h1><blink>hello!</h1>";
 <body bgcolor="#339933">
 
   <DIV align=center><FONT style="FONT-SIZE: 24pt; FILTER: shadow(color=green direction=45); WIDTH: 100%;
-COLOR: #FFCC00; LINE-HEIGHT: 150%"><B>HAHAHAH</B></FONT></DIV>
+COLOR: #FFCC00; LINE-HEIGHT: 150%"><B>HAHAHA是H</B></FONT></DIV>
  </body>
 </html>
