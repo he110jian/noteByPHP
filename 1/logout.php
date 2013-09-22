ae7b06949854261a6e9b635d0717f802cb6546d3
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-22 17:52:13 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/logout.php

--- 1/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-setcookie("cookie"); 
-	echo "<script language=\"javascript\"> alert('log out successfully!');</script>"; 
- 	echo "<script language=\"javascript\">location.href='index.php';</script>";
-?> 
