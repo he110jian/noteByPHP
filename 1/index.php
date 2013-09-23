6607a1fdb343f97c830a5fe140ec2e1179e865ce
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 05:08:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Login</title>
     </head>
-    <body>
+    <body bgcolor="#efdeb0">
 <?php
 if(!isset($_COOKIE["login"]))
 {
