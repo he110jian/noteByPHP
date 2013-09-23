bcdee003549d053257c1d0bbe62b072b5e7833b9
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 09:50:35 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/hello.php

--- 1/hello.php
+++ 1/hello.php
@@ -11,7 +11,7 @@ if(isset($_COOKIE["login"]))
         <script src="js/prototype.lite.js" type="text/javascript"></script>
 <link href="js/type.css" type="text/css" rel="stylesheet"  />
     </head>
-        <body style="font-color:white">
+        <body style="font color:white">
 <div id="div1"><img src="../js/1.jpg" /></div>   
         <body>
 <div id="container">
