38407bf3f764c46988077e382e2aa093fada5d89
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 02:55:37 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/log.php

--- 1/log.php
+++ 1/log.php
@@ -3,12 +3,7 @@ define(ALL_PS,"binggo");   //
 //=======================================登录
  if($_POST[sub])
  {
- 	$username=str_replace(" ","",$_POST[username]);
- 	if($username=="jean")
-        $us=TRUE;
- 	$ps=md5($_POST[password].ALL_PS)=="e18f8200bbe2fb0b0176a3b5c6228d3e";
- 	$s=$us ? $ps : FALSE;
- 	if($s)
+ 	if($ps)
     {
  		setcookie("cookie","ok",time()+1800);
  		echo "<script language=\"javascript\">location.href='hello.php';</script>";
