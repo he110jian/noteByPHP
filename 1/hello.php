83ef080265655f9363b6a3cb291b13cde1f2f871
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 09:40:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/hello.php

--- 1/hello.php
+++ 1/hello.php
@@ -1,6 +1,4 @@
-
 <?php
-//判断登录
 if(isset($_COOKIE["login"]))
 {
     include_once("head.php");
@@ -12,10 +10,31 @@ if(isset($_COOKIE["login"]))
              <title>Home</title>
         <script src="js/prototype.lite.js" type="text/javascript"></script>
 <link href="js/type.css" type="text/css" rel="stylesheet"  />
-    </head><body style="color:white;font-family:verdana">
+    </head><body>
 <div id="div1"><img src="../js/1.jpg" /></div>   
-        <br/><hr/>
-        <div>
+        <body>
+<div id="container">
+  <div id="header">This is the Header</div>
+  <br class="clearfloat" />
+  <div id="menu">This is the Menu</div>
+  <br class="clearfloat" />
+  <div id="mainContent">
+    <div id="sidebar">This is the sidebar<br />
+      <br />
+    This is the sidebar<br />
+      <br />
+    This is the sidebar<br />
+      <br />
+This is the sidebar<br />
+<br />
+This is the sidebar<br />
+<br />
+This is the sidebar<br />
+<br />
+This is the sidebar<br />
+<br />
+    </div>
+    <div id="content">2列右窄左宽、高度自适应且未知高度底部平齐+头部+导航+尾部——<a href="http://www.aa25.cn">标准之路www.aa25.cn</a><br />
 		<h4>
 		Done In The First Week @2013.9.2-2013.9.8:
 		</h4>
@@ -47,7 +66,11 @@ if(isset($_COOKIE["login"]))
 		<h5>
 		--By HeJian
 		</h5>
-	</div>
+    </div>
+  </div>
+  <br class="clearfloat" />
+  <div id="footer">This is the footer</div>
+</div>
 </body></html>
 <?
 } 
