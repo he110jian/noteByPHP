a91453ab9fb3a3671e2139fcecd6832b9540ff49
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 05:00:56 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/head.php

--- 1/head.php
+++ 1/head.php
@@ -1,2 +1,5 @@
 
-    <a href='log.php?out=out'><span style="float:right;">Logout<hr></span></a>
+    <a href='log.php?out=out'>LogOut</a><hr><
+<div class="text clearfix" style="text-align:center;">
+<span style="float:right;">居右</span><span style="float:left;">居左</span>居中
+</div>
