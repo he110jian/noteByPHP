b5fea2dc53989d136ee8ca1f31377d78bdcdf04a
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 02:58:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/hello.php

--- 1/hello.php
+++ 1/hello.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-         <title>Home</title>
-    </head>
-    <body>
+
 <?php
 session_start();
 //判断登录
@@ -15,8 +9,15 @@ if(isset($_COOKIE['cookie']))
 else 
 {   
 include_once("head.php");
-echo "<H1>WELCOME!<H1>";
+?>"<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+         <title>Home</title>
+    </head>
+    <body><H1>WELCOME!<H1></body></html>";
+        <?
 }
 ?>
 
-    </body></html>
+    
