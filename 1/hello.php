bd5ce3c858f46f2177dd4aba2012d4903c383b47
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-23 04:21:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/hello.php

--- 1/hello.php
+++ 1/hello.php
@@ -1,21 +1,21 @@
 
 <?php
 //判断登录
-if(!isset($_COOKIE['login']))
+if(isset($_COOKIE['login']))
 {
-	echo "<meta http-equiv=\"refresh\" content=\"3;url=index.php\"> illegal!<a href=\"index.php\">return</a>...";
+    include_once("head.php");
+    ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+             <title>Home</title>
+        </head>
+        <body><H1>WELCOME 您!<H1></body></html>
+            <?
 } 
 else 
 {   
-include_once("head.php");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-         <title>Home</title>
-    </head>
-    <body><H1>WELCOME 您!<H1></body></html>
-        <?
+	echo "<meta http-equiv=\"refresh\" content=\"3;url=index.php\"> illegal!<a href=\"index.php\">return</a>...";
 }
 ?>
