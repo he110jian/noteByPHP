94e4df87bc34adf02b6d3877a0592e2db0cff5c6
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-24 09:27:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/foot.php

--- 1/foot.php
+++ 1/foot.php
@@ -4,13 +4,14 @@ if(isset($_COOKIE["login"]))
 //	include_once("head.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=gb2312">
-
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="text/javascript" src="./js2/mootools.js"></script>
 <script type="text/javascript" src="./js2/accordions.js"></script>
 <link href="./js2/accordions.css" type="text/css" rel="stylesheet"  />
 
-<title>Accordio</title>
+<title>PersonInfo</title>
 </head>
 <body>
 <br/><br/><br/>
