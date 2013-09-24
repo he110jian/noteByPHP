801596bdda088af9ae3a45fa99cc012f0756310d
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-24 15:25:16 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/foot.php

--- 1/foot.php
+++ 1/foot.php
@@ -30,9 +30,9 @@ if(isset($_COOKIE["login"]))
   }
 </script>
 <div align="center">
-        <form name="form" action="#" method="post" onsubmit="return Check()" >
+
   <input style="WIDTH: 360px; HEIGHT:30px" type="text" name="things" />
-  </form>
+  <input type="hidden" tybe="button" name="sub1" value="" onclick()=Check()/>
         <br/>
     </div>
 <div>
