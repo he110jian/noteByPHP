0373d1f034e08d0fe745ea7f4cea138a0a384f2d
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-24 09:51:23 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/foot.php

--- 1/foot.php
+++ 1/foot.php
@@ -17,7 +17,7 @@ if(isset($_COOKIE["login"]))
 <br/><br/><br/>
     <div align="center">
         <form name="form2" action="log.php" method="post" >
-  <input type="text" name="things" />
+  <input style="WIDTH: 344px; HEIGHT: 21px" type="text" name="things" />
   <input type="hidden" name="sub1" value="sub1" />
   </form>
     </div>
