78c270ccdca561211ff90724421b3fe791fa5b85
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-26 06:54:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/del.php

---
+++ 1/del.php
@@ -0,0 +1,26 @@
+<?
+    $dom=new DOMDocument; 
+    $dom->load("data.xml");      
+    $root=$dom->getElementsByTagName("messages"); 
+    $root=$root->item(0);
+ foreach($root->childNodes as $msg)
+ {
+  if($msg->firstChild->nodeValue==$_GET["id"])   //如果message节点的id子节点的值跟要删除的id相等
+  {
+   $photo=$msg->lastChild->nodeValue;
+   if($photo!="NONE"){   //如果留言包含图片，还应该将图片删除
+    $photo_path="upfile/".base64_decode($photo);
+    $flag=unlink($photo_path);
+    if($flag){
+     echo "删除图片成功<br>";
+    }
+   }
+   $root->removeChild($msg);
+   break;
+  }
+ }
+ $dom->save("data.xml");
+
+?>
+删除留言成功，2秒钟返回首页
+<meta http-equiv="refresh" content="2;url=foot.php">
