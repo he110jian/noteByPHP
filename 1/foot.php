5eb41b8249bf07664966f3dc9f8d932d3a92651d
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-26 06:28:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/foot.php

--- 1/foot.php
+++ 1/foot.php
@@ -14,43 +14,74 @@ if(isset($_COOKIE["login"]))
 <title>PersonInfo</title>
 </head>
 <body>
-<br/><br/><br/> 
-    <script language="JavaScript" type="text/javascript">
-  function Check(){
-  	if(form.things.value=="")
-    {
-  		alert("write someting!");
-  		form.things.focus();
-  		return false;
-  	}
-  	else
-    {
-        document.getElementById("p1").innerText = "asasa";
-  	}
-  }
-</script>
-<div align="center">
+<br/>
+    <p><a href="add.php">添加留言</a></p>
+<?php
+$dom=new DOMDocument('1.0','gb2312');   
+$dom->load("data.xml");       //加载
+$root=$dom->getElementsByTagName("messages"); 
+$root=$root->item(0);      
+$message=$root->getElementsByTagName("message");   //获取所有message节点
+$message_count=$message->length;   //计算有多少条留言
+echo "当前共有".$message_count."条留言";
+if($message_count==0){
+ echo "暂时没有留言\n";
+}else{
+?>
 
-  <input style="WIDTH: 360px; HEIGHT:30px" type="text" name="things" />
-        <br/>
-    </div>
+<br/><br/> 
+
 <div>
-<p class="accToggler" style="background-color: rgb(194, 220, 114); " >goal</p>
+    <p class="accToggler" style="background-color: rgb(194, 220, 114); ">about</p>
 	<p id="p1" class="accContent" style="overflow-x: hidden; overflow-y: hidden; visibility: visible; height: 120px; opacity: 1;">
-        l
+		detatil
 	</p>
-<p class="accToggler" style="background-color: rgb(194, 220, 114); ">项目经历</p>
-	<p class="accContent" style="overflow-x: hidden; overflow-y: hidden; visibility: visible; height: 120px; opacity: 1;">
-		a2dddddddddsadddddddddddddddsadddddddddddddddsadddddddddddddddsadddddddddddsdasdaaaaaaaaaaaaaaaaasadddddddddddsadddd
-		dddddddsadddddddddddsadddddddddddddddsadddddddddddddddsadddddddddddddddsaddddddddddd
-		dddddddsadddddddddddsaddd
+        <?php
+ for($i=$message_count-1;$i>=0;$i--)    //我们需要对留言按倒序排列
+ {
+  $msg=$message->item($i);
+
+  foreach($msg->childNodes as $child)   //message节点的各个子节点
+  {
+   if($child->nodeName=="id")
+   {
+    $id=$child->nodeValue;
+   }
+   if($child->nodeName=="author")
+   {
+    $author=$child->nodeValue;
+   }
+   if($child->nodeName=="title")
+   {
+    $title=$child->nodeValue;
+   }
+   if($child->nodeName=="content")
+   {
+    $content=$child->nodeValue;
+   }
+   if($child->nodeName=="photo")
+   {
+    $photo=$child->nodeValue;
+   }
+   if($child->nodeName=="addtime")
+   {
+    $addtime=$child->nodeValue;
+   }
+
+  }?>
+<p class="accToggler" style="background-color: rgb(194, 220, 114); " ><?echo $id.">".base64_decode($title)." - ".base64_decode($author)." [".$addtime."] ";  
+   echo "[<a href='del.php?id=".$id."'>删除</a>]";?>
+</p>
+    <p class="accContent" style="overflow-x: hidden; overflow-y: hidden; visibility: visible; height: 120px; opacity: 1;">
+	
+ <?       echo "<tr><td align=left>".base64_decode($content)."</td></tr>";
+  if($photo!="NONE")
+  {
+      echo "<br/><img src='upfile/".base64_decode($photo)."'>";
+  }
+ }?>
 	</p>
-<p class="accToggler" style="background-color: rgb(194, 220, 114); ">项目经历</p>
-	<p class="accContent" style="overflow-x: hidden; overflow-y: hidden; visibility: visible; height: 120px; opacity: 1;">
-		a2dddddddddsadddddddddddddddsadddddddddddddddsadddddddddddddddsadddddddddddsdasdaaaaaaaaaaaaaaaaasadddddddddddsadddd
-		dddddddsadddddddddddsadddddddddddddddsadddddddddddddddsadddddddddddddddsaddddddddddd
-		dddddddsadddddddddddsaddd
-	</p>
+
 </div>
 	</body>
 	</html>
