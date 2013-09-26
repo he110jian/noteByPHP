8c11661f081ef97dc8f854db52f2e94e3015563a
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-09-26 07:07:44 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/foot.php

--- 1/foot.php
+++ 1/foot.php
@@ -85,13 +85,37 @@ if($message_count==0){
 	</p>
 
 </div>    <div id="footer">&nbsp;
-    <script type="text/javascript">
-var d =new Date();
-with(d)
-{
-    document.write((getFullYear()+"年"+(getMonth()+1)+"月"+getDate()+"日星期"+getDay())+"<br/>&nbsp;&nbsp;"+getHours()+":"+getMinutes()+":"+getSeconds());
-}
-</script>
+   <script type="text/JavaScript">
+   					 document.write("<span id='clock'></span>");
+  					 var Digital,hours,minutes,seconds,timeValue,years,months,dates,day,Dday;
+  					 function showtime()
+   					 {
+    					Digital=new Date();
+						years=Digital.getFullYear();
+						months=Digital.getMonth();
+						dates=Digital.getDate();
+						day=Digital.getDay();
+						Dday=new Array("日","一","二","三","四","五","六");
+						hours = Digital.getHours();
+						minutes = Digital.getMinutes();
+						seconds = Digital.getSeconds();
+						months=months+1;
+						if(months<=9)
+							months="0"+months;
+						if(dates<=9)
+							dates="0"+dates;
+						if(hours<=9)
+							hours="0"+hours;
+	   					timeValue=years+"年"+months+"月"+dates+"日 星期"+Dday[day]+"<br>&nbsp;";
+						timeValue += (hours >= 12) ? "下午 " : "上午 ";
+						timeValue += hours + "点";
+						timeValue += ((minutes <10)?"0":"") + minutes+"分";
+						timeValue += ((seconds <10)?"0":"") + seconds+"秒";
+						clock.innerHTML = timeValue;
+						setTimeout("showtime()",1000);
+   					 }
+    				showtime()
+				</script>
     </div>
 	</body>
 	</html>
