50d04c9c9f8f2b357809d839b46d9275ce6e15b7
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-10-05 12:32:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/add.php

--- 1/add.php
+++ 1/add.php
@@ -6,12 +6,27 @@ if(isset($_COOKIE["login"]))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="js/type.css" type="text/css" rel="stylesheet"  />
+        <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
 <title>Add</title>
     </head>
     <body>
-<form action="saveadd.php" enctype="multipart/form-data" method="post" name="myform" onsubmit="return go(this)">
-<table border="1" width="600">
+	        <div id="div1"><img src="js/recbg.jpg" /></div>  
+	<br/><br/>
+	<script language="JavaScript" type="text/javascript">
+  function Check(){
+  	if(myform.content.value==""){
+  		alert("请认真填写!");
+  		myform.content.focus();
+  		return false;
+  }
+}
+</script>
+<fieldset>
+<legend>Create</legend>
+	<div style="text-align:center;width:100%;height:100%">
+<form action="saveadd.php" enctype="multipart/form-data" method="post" name="myform" onsubmit="return Check()">
+ <table width="100%" border="0" align="center" cellpadding="4" cellspacing="4" bordercolor="#666666">
  <tr>
   <td>作者</td>
   <td align="left"><input type="text" name="author" size="10"></td>
@@ -29,7 +44,7 @@ if(isset($_COOKIE["login"]))
   <td align="left"><input type="file" name="upfile" size="50"></td>
  </tr>
  <tr>
-  <td colspan="2"><input type="submit" value="提交"/></td>
+  <td colspan="2"><input type="submit" value=" 提 交 " onMouseOver=this.style.color="red"/></td>
  </tr>
 </table>
 </form>
@@ -40,5 +55,7 @@ else
 	echo "<meta http-equiv=\"refresh\" content=\"3;url=index.php\"> illegal!<a href=\"index.php\">return</a>...";
 }
 ?>
+</div>
+</fieldset>
 </body>
 </html>
