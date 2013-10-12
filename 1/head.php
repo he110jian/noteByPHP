d147c56a0a441bfb0ef8479629a1377d6f28561c
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-10-12 14:44:29 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/head.php

--- 1/head.php
+++ 1/head.php
@@ -1,6 +1,16 @@
-<html>
-    <body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ho</title>
+</head>
+<body>
 <span style="float:right;"> <a href='log.php?out=out'>LogOut</a></span>
-
+<?
+$domain =  "hellojian";
+$filename = "data.xml";
+$flag = fileExists(string $domain, string $filename);
+echo $flag;
+?>
 </body>
 </html>
