728024e80278a34e046363f29e5d0a8e2d4369ab
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-10-12 14:44:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/head.php

--- 1/head.php
+++ 1/head.php
@@ -6,11 +6,6 @@
 </head>
 <body>
 <span style="float:right;"> <a href='log.php?out=out'>LogOut</a></span>
-<?
-$domain =  "hellojian";
-$filename = "data.xml";
-$flag = fileExists(string $domain, string $filename);
-echo $flag;
-?>
+
 </body>
 </html>
