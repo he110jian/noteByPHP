3f066fa9680b309eed0fd6654a833b093d1854cb
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2013-10-12 15:57:23 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/foot.php

--- 1/foot.php
+++ 1/foot.php
@@ -22,6 +22,7 @@ if(isset($_COOKIE["login"]))
 
         <div id="div1"><img src="js/public-bg9.jpg" /></div>   
         <div id="right"><div class="shortcut-bg"></div><div class="shortcut-text"><a href="http://baidu.com" target="_blank">百度一下</a></div></div>
+    <div>
         <div id="header-bg"></div>
         <div id="header-fading"></div> 
 <div style="top: 20px;
@@ -62,7 +63,7 @@ text-align: center;">
     				showtime()
 				</script>
 </div>
-
+    </div>
 <?php
 $dom=new DOMDocument('1.0','gb2312');   
 $dom->load("data.xml");       //加载
