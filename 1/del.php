a052276f0a8733af1e36172c4fa197dc426ee4f2
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-16 16:04:36 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/del.php

--- 1/del.php
+++ 1/del.php
@@ -9,7 +9,7 @@ $content = $_POST['content'];
 $title= trim($_POST["title"]);
     //$time = date("Y-m-d H:i:s");
 $fielPath="NONE";
-$sql = "INSERT  INTO `note` ( `title`, `content`, `time`) VALUES ('"  . $mysql->escape( $title ) . "' , '" . $mysql->escape( $content ) . "' , NOW() ) ";
+$sql = "delete from note where LastName='Griffin'";
 $mysql->runSql($sql);
 if ($mysql->errno() != 0)
 {
