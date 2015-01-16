b3f4c729f3e7a2835dba81b461405a3e76090a44
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-16 16:15:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/del.php

--- 1/del.php
+++ 1/del.php
@@ -9,7 +9,7 @@ $content = $_POST['content'];
 $title= trim($_POST["title"]);
     //$time = date("Y-m-d H:i:s");
 $fielPath="NONE";
-$sql = "delete from note where LastName='Griffin'";
+$sql = "delete from note where LastName='".$_GET["id"]."'";
 $mysql->runSql($sql);
 if ($mysql->errno() != 0)
 {
@@ -18,10 +18,32 @@ if ($mysql->errno() != 0)
 
 $mysql->closeDb();
 
-echo "<meta http-equiv=\"refresh\" content=\"0;url=note.php\">\n";
+echo '1';
 }
-else
+?>
+
+<?php
+if(isset($_COOKIE["login"]))
 {
-	header("location:note.php");
+	$dom=new DOMDocument;
+    $dom->load("medias/data.xml");
+    $root=$dom->getElementsByTagName("messages"); 
+    $root=$root->item(0);
+	foreach($root->childNodes as $msg)
+	{
+		if($msg->firstChild->nodeValue==$_GET["id"])   //如果message节点的id子节点的值跟要删除的id相等
+		{
+			$photo=$msg->lastChild->nodeValue;
+			if($photo!="NONE")
+			{   //如果留言包含图片，还应该将图片删除
+				$photo_path="medias/upfile/".base64_decode($photo);
+				unlink($photo_path);
+			}
+		$root->removeChild($msg);
+		break;
+		}
+	}
+	$dom->save("medias/data.xml");
+	
 }
 ?>
