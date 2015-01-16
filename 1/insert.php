e4c363cf49de0296b215adb3a24e48b23db6c7ab
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-16 15:59:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/insert.php

--- 1/insert.php
+++ 1/insert.php
@@ -10,7 +10,7 @@ $content = $_POST['content'];
 $title= trim($_POST["title"]);
     //$time = date("Y-m-d H:i:s");
 $fielPath="NONE";
-$sql = "INSERT  INTO `note` ( `title`, `cotent`, `time`) VALUES ('"  . $mysql->escape( $title ) . "' , '" . $mysql->escape( $content ) . "' , NOW() ) ";
+$sql = "INSERT  INTO `note` ( `title`, `content`, `time`) VALUES ('"  . $mysql->escape( $title ) . "' , '" . $mysql->escape( $content ) . "' , NOW() ) ";
 $mysql->runSql($sql);
 if ($mysql->errno() != 0)
 {
