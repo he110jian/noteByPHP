e2630398c30f586509978678a6d2ee7d020f8fca
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-16 17:10:01 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -140,11 +140,6 @@ else
 		xmlhttp.open("GET","del.php?id="+str,true);
 		xmlhttp.send();
 	}
-	function fix(str)
-	{
-		$("#"+str).toggleClass( "in fixthis", 1000 );
-		$("#fix"+str).toggleClass( "text-muted", 1000 );
-	}
 	$(window).scroll(function() {
 		$("#leaveMSG").stop().animate({ top: $(window).scrollTop() }, "slow");
   });
