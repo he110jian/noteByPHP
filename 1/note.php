ae34f78228a117d0cc4d231cab9c7e2aaf38c32b
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-16 17:08:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -21,8 +21,13 @@ define(ALL_PS,"binggo");   //
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Remember</title>
-<link href="css/my.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <link href="css/my.css" rel="stylesheet">
 
 </head>
