d19f1c99ed18627ec9272c4186077f3a2fef3195
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-16 16:44:45 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/insert.php

--- 1/insert.php
+++ 1/insert.php
@@ -10,17 +10,8 @@ $title= trim($_POST["title"]);
     //$time = date("Y-m-d H:i:s");
 $fielPath="NONE";
     
-$sql = "SELECT title from note where title = ".$title." limit 1";
-$sql = "SELECT * FROM `note` order by time asc"; 
-$data = $mysql->getData( $sql );
-if(count($data))
-{
-    // $sql = "UPDATE note set content=".$content.",time=NOW() where title=".$title;
-}
-    else
-    {
 		$sql = "INSERT  INTO `note` ( `title`, `content`, `time`) VALUES ('"  . $mysql->escape( $title ) . "' , '" . $mysql->escape( $content ) . "' , NOW() ) ";
-    }
+
 $mysql->runSql($sql);
 if ($mysql->errno() != 0)
 {
