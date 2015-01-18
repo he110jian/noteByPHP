e5156102d834c775558b5b91f9ef919ad045dc0a
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-18 15:20:31 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/insert.php

--- 1/insert.php
+++ 1/insert.php
@@ -6,7 +6,7 @@ if(isset($_COOKIE["login"]) && $_POST['title']!=NULL)
 $mysql = new SaeMysql();
 
     //$title=addslashes(trim($_POST["title"]));
-$_POST = json_encode($_POST);
+
 $content = $_POST['content'];
 $title= trim($_POST["title"]);
 if(1 == $_POST["options"])
@@ -16,7 +16,7 @@ if(1 == $_POST["options"])
 	$content = str_replace('<',"&lt;",$content);
 	$content = str_replace('>',"&gt;",$content);
 }
-    $content=str_replace("\r\n","<br/>",$content);
+   $content=str_replace("\r\n","<br>",$content);
     //$time = date("Y-m-d H:i:s");
 $fielPath="NONE";
 $sql = "SELECT * FROM `note` where title='".$title."' limit 1"; 
