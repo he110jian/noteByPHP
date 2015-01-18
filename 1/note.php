4931fd13276253a2d1387c7c5518dc589d72ef35
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-18 12:21:18 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -62,7 +62,7 @@ define(ALL_PS,"binggo");   //
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <h4 class="modal-title">For Remember</h4>
       </div>
-	  <form role="form" enctype="multipart/form-data" id="megform" onsubmit="sub()">
+	  <form role="form" enctype="multipart/form-data" method="post" action="insert.php" onsubmit="document.getElementById('remember').value = 'Uploading...'">
       <div class="modal-body">
 		<div class="form-group">
 			<label>主题</label>
@@ -103,10 +103,6 @@ else
 }
 ?>
 <script language="javascript">
-    function sub()
-    {
-        document.getElementById('remember').value = 'Uploading...';
-    }
     function delcfm(str) {
 		var f = confirm("Never Mind?");
 		if(f){
