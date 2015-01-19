33d0c7407676c561c8317be00ba1fa8af2a73e63
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-19 12:12:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -119,7 +119,7 @@ else
                     alert(msg.content);
                     
                     document.getElementById('remember').value = 'Save Message';
-                    $('#myModal').modal('fade');
+                    $('#myModal').modal('hide');
                     var i = $("#count");
 					i.text(parseInt(i.text())+1);
                 }
