6ed5fb3da4f4e7678a8895cf13106cf1912fa6bc
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-19 12:57:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/insert.php

--- 1/insert.php
+++ 1/insert.php
@@ -24,7 +24,7 @@ $data = $mysql->getData( $sql );
 $mysql->runSql($sql);
 $inse = true;
 $update = -1;
-for($i=0;$i<count($data);$i--)
+for($i=0;$i<count($data);$i++)
 {
 	$msg=$data[$i];
 	if(!strcmp($title,$msg['title']))
