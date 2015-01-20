c9f463c6575a5d80b3c11a1b4514a427dedfee94
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-20 09:52:53 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -121,10 +121,10 @@ else
                     $("#contentE").val('');
                     $("#fileE").val('');
                     var pic = "";
-                    var i = $("#count");
+                    var v = $("#count");
                     if(msg.update===-1)
                     {
-                    	i = i.text();
+                    	i = v.text();
                     }
                     else
                     {
@@ -147,7 +147,7 @@ else
                         var tail = "<p class='text-center'><a href='javascript:void(0);' onclick='edit("+i+")' class='pull-left text-info'><span class='glyphicon glyphicon-edit'></span></a><a class='pull-right text-danger' href='javascript:void(0);' onclick=\"return delcfm(\'"+msg.time+"\');\"><span class='glyphicon glyphicon-remove'></span></a></p></div></div></div>";
                         add = add + pic + tail;
                         $("#accordion").prepend(add);
-                        i.text(parseInt(i.text())+1);
+                        v.text(parseInt(v.text())+1);
                     }
                     else
                     {
