688d544dbb607820179ec58c75a059049e0b918a
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-20 14:28:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/musicList.php

--- 1/musicList.php
+++ 1/musicList.php
@@ -1,3 +1,12 @@
-<?php
-
+<?php 
+if($_POST['submit'])
+{
+    print_r($_POST);
+    echo"<hr/>";
+}
 ?>
+<form action="" method="post" target="_blank">
+  Add one: <input type="text" name="fname"><br>
+  Delete one: <input type="text" name="lname"><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
