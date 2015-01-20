004f991bf5de75d7b82d5438b754f00b98f22acc
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-20 11:46:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -5,7 +5,7 @@ if($_POST[sub])
  	$ps=md5($_POST[password].ALL_PS)=="e18f8200bbe2fb0b0176a3b5c6228d3e";
  	if($ps)
     {
- 		setcookie("login","ok",time()+3600);
+ 		setcookie("login","ok",time()+3600*2);
 		header("Location:note.php");
  	}
  }
