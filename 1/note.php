7e02cb588bac3b9a2b1f922c0bb133466625d13a
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-20 05:43:10 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -107,7 +107,7 @@ else
 <script language="javascript">
         $('form').bind('submit', function(){
             document.getElementById('remember').value = 'Uploading...';
-            var params = $("form").serialize();alert(params);
+            var params = $("form").serialize();
             var url = "insert.php";
             $.ajax({
                 type: "post",
