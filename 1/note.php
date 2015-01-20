1007df37d3f2087f06e65897a3a90c652da2f085
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-20 09:00:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -181,9 +181,9 @@ else
 		msg_v = msg.innerText;
 		des = document.getElementById("titleE");
 		des.value = msg_v;
-        msg = document.getElementById("file"+id).href;
-        //des = document.getElementById("fileE");
-        //des.value = msg;
+        //msg = document.getElementById("file"+id).href;
+        des = document.getElementById("fileE");
+		des.value = "as";
 		$('#myModal').modal('show');
 	}
 	function showChange(str) {
