8e00476ade91472d7c9a7d9fafc9cd18a4d5d85f
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-20 09:35:26 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -121,6 +121,16 @@ else
                     $("#contentE").val('');
                     $("#fileE").val('');
                     var pic = "";
+                    var i = $("#count");
+                    if(msg.update===-1)
+                    {
+                        i.text(parseInt(i.text())+1);
+                    	i = i.text();
+                    }
+                    else
+                    {
+                        i=msg.update;
+                    }
                     if(msg.filePath)
                     {
                             if(msg.pic)
@@ -134,9 +144,6 @@ else
                     }
                     if(msg.update===-1)
                     {
-                    	var i = $("#count");
-						i.text(parseInt(i.text())+1);
-                        i = i.text();
                         var add = "<div class='panel panel-default' id='"+msg.time+"'><div class='panel-heading' role='tab' id='headingOne'><h4 class='panel-title'><a data-toggle='collapse' data-parent='#accordion' href='#collapse"+i+"'><span  class='glyphicon glyphicon-circle-arrow-right'></span><span id='title"+i+"'> "+msg.title+"</span><small class='pull-right text-muted'>"+msg.time+"</small></a></h4></div><div id='collapse"+i+"' class='panel-collapse collapse in'><div class='panel-body'><p id='msg"+i+"'>"+msg.content+"</p>";
                         var tail = "<hr/><p class='text-center'><a href='javascript:void(0);' onclick='edit("+i+")' class='pull-left text-info'><span class='glyphicon glyphicon-edit'></span></a><a class='pull-right text-danger' href='javascript:void(0);' onclick=\"return delcfm(\'"+msg.time+"\');\"><span class='glyphicon glyphicon-remove'></span></a></p></div></div></div>";
                         add = add + pic + tail;
