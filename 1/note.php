31aff375690ab7c1a55851ecf97e26be0763ff8c
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-21 09:27:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -14,7 +14,7 @@ if(isset($_COOKIE["login"]))
 </head>
 <!-- NAVBAR
 ================================================== -->
-
+<body>
 <?php 
 			$id = rand(0,46);
 			$st = new SaeStorage();
@@ -26,7 +26,7 @@ if(isset($_COOKIE["login"]))
 			$last = $last[$id];
 ?>
     <div class="s-skin-container" style="background-color:rgb(64, 64, 64);background-image:url(http://images.wisegeek.com/images/dyk/<?php echo $last;?>.jpg);"></div>
-    <body>
+    
     <div class="container"> 
 		<div class="middlediv">
             <div id="music">
