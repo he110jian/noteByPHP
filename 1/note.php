f16f553460a32ef54914609b16d47bb65ae29607
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-21 09:55:09 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -17,6 +17,7 @@ if(isset($_COOKIE["login"]))
 <body>
     <script>
         alert(document.body.clientWidth ); //浏览器当前窗口文档的高度 
+       alert(document.body.clientHeight);
     </script>
 <?php 
 			$id = rand(0,46);
