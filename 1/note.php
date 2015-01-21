d4fbfc14c171ce91368ee734b088af6800a60c06
908984017@qq.com <908984017@qq.com@9dbdc04c-c6f3-4a0b-9043-a84f5b28877c>
2015-01-21 08:45:12 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/note.php

--- 1/note.php
+++ 1/note.php
@@ -25,7 +25,7 @@ if(isset($_COOKIE["login"]))
 			$last = explode("\r\n",$content);
 			$last = $last[$id];
 ?>
-<body style="background: #00FF00 url('http://images.wisegeek.com/images/dyk/<?php echo $last;?>.jpg') no-repeat top">
+<body style="background-attachment: fixed; background: url('http://images.wisegeek.com/images/dyk/<?php echo $last;?>.jpg') no-repeat">
     <div class="container"> 
 		<div class="middlediv">
             <div id="music">
